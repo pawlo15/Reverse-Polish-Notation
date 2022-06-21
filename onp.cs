58bc64b71b5ab6134741559378647ca66bd4e982
Paul <95492170+pawlo15@users.noreply.github.com>
2022-06-21 19:11:25 +0200

    Create onp.cs

---
+++ onp.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+
+namespace onp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Stack stos = new Stack();
+            string tmp = Console.ReadLine();
+            Console.WriteLine("Przekształcenie na ONP:");
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                switch (tmp[i])
+                {
+                    case ' ':
+                        break;
+
+                    case '(':
+                        stos.Push('(');
+                        break;
+
+                    case ')':
+                        while ((char)stos.Peek() != '(')
+                        {
+                            Console.Write(stos.Pop() + " ");
+                        }
+                        stos.Pop();
+                        break;
+
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                    case '^':
+                        while (stos.Count != 0)
+                        {
+                            if ((Priorytet(tmp[i]) == 3) || (Priorytet(tmp[i]) > Priorytet((char)stos.Peek())))
+                            {
+                                break;
+                            }
+                            if ((char)stos.Peek()!='(')
+                            {
+                                Console.Write(stos.Pop() + " ");
+                            }
+                        }                
+                        stos.Push(tmp[i]);
+                        break;
+                    default:
+                        Console.Write(tmp[i] + " ");
+                        break;
+
+                }
+            }
+            PrintValues(stos);
+            Console.ReadLine();
+        }
+        
+        public static void PrintValues(IEnumerable myCollection) // wypisanie
+        {
+            foreach (Object obj in myCollection)
+                if ((char)obj !='(')
+                {
+                    Console.Write("{0} ", obj);
+                }
+            Console.WriteLine();
+        }
+        
+        static int Priorytet(char x) // sprawdzenie priorytetu
+        {
+            switch (x)
+            {
+                case '+':
+                    return 1;
+                case '-':
+                    return 1;
+                case '*':
+                    return 2;
+                case '/':
+                    return 2;
+                case '^':
+                    return 3;
+                default:
+                    return -1;
+            }
+        }
+    }
+}
